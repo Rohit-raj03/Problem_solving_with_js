df1acd87bb63f0ddf70de7e71d7607f2045a1115
rohit raj <rohitrajchy03@gmail.com>
2026-09-16 23:12:30 +0530

    Day01 practice question

---
+++ DAY01/script.js
@@ -0,0 +1,13 @@
+// Print Numbers from 1 to N
+const num = 5;
+
+for (let i = 1; i <= num; i++) {
+  console.log(i);
+}
+
+// Print Numbers from N to 1 without changing the loop condition of above question
+// const num = 5;
+
+for (let i = 1; i <= num; i++) {
+  console.log(i - 1 + 1);
+}
